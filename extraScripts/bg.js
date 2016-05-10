21bd2320d70233431c9b4065854458736bc6feb6
rajatsharma305 <lunasunkaiser@gmail.com>
2016-05-10 23:14:54 +0530

    season6episode3

--- extraScripts/bg.js
+++ extraScripts/bg.js
@@ -1,6 +1,6 @@
 $("#example, body").vegas({
     slides: [
-        { src: 'http://i.lv3.hbo.com/assets/images/series/game-of-thrones/episodes/6/52/episode-52-1024.jpg' }
+        { src: 'http://i.lv3.hbo.com/assets/images/series/game-of-thrones/episodes/6/53/episode-53-1024.jpg' }
     ],
     overlay: true
 });
